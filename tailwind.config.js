24371a9ee58afa34527dfd41d3abe56eded1b0eb
Sejoqariz <sejokarizz@gmail.com>
2024-11-07 09:30:06 +0000

    initilize react app

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        cyan: '#00FFFF',
+        orange: '#FFA500',
+        darkBg: '#1a1a2e',
+        darkCalc: '#162447'
+      }
+    }
+  },
+  plugins: []
+}
